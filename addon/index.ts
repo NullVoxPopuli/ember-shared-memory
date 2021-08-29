e483fb1d8a8c0745178aa7eb64cf264e0f1ab593
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-08-29 16:25:29 -0400

    wip

--- addon/index.ts
+++ addon/index.ts
@@ -1,20 +1,61 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/ban-types */
 type Stringy = string | string[] | Array<{ toString: () => string }>;
 
-// export function shared(keyFn: () => Stringy) => PropertyDecorator;
-// export function shared(key: object | string) => PropertyDecorator;
-// export function shared  PropertyDecorator;
+type Key = string | Stringy;
 
-export const shared  = (...args: any[]): void | PropertyDecorator {
-  if (args.length === 1 ) {
-    return (target, key, descriptor) => {
-      // decorator
-    }
+type Decorator = (
+  target: object,
+  key: string | symbol,
+  descriptor: PropertyDescriptor
+) => PropertyDescriptor;
+
+type DecoratorFactory = Decorator | ((key: Key) => Decorator);
+
+export function shared(target: object, key: string | symbol, descriptor: PropertyDescriptor): void;
+export function shared(
+  key: Key
+): (target: object, key: string | symbol, descriptor: PropertyDescriptor) => void;
+
+/**
+ *
+ */
+export function shared(...args: any[]): ReturnType<DecoratorFactory> {
+  if (args.length === 1) {
+    let decorator: Decorator = (target, propName, descriptor) => {
+      return decoratorCachedVia(target, propName, descriptor, args[0]);
+    };
+
+    return decorator;
   }
 
-  if (args.length === 3) {
-    let [target, key, descriptor]  = args;
+  if (args.length > 1) {
+    let [target, propName, descriptor] = args;
+
+    return decoratorCachedVia(target, propName, descriptor);
   }
 
-  throw new Error('Incorrect Arity for @shared');
+  throw new Error(
+    `Incorrect Arity for @shared. Expected either 1 or 3 (decorator), but received: ${args.length}`
+  );
+}
+
+function decoratorCachedVia(
+  target: object,
+  propName: string | symbol,
+  descriptor: PropertyDescriptor,
+  key?: Key
+) {
+  let oldGet = descriptor.get;
+
+  console.log({ target, propName, key });
+
+  descriptor.get = function () {
+    console.log({ target, propName, key });
+    console.log(this.constructor);
+
+    return oldGet();
+  };
+
+  return descriptor;
 }
--- tests/unit/shared-test.ts
+++ tests/unit/shared-test.ts
@@ -1,29 +1,25 @@
-import { setOwner } from '@ember/application';
-import { settled } from '@ember/test-helpers';
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
 import { shared } from 'ember-shared-memory';
 
-module('shared', function () {
-    setupTest(hooks);
+module('shared', function (hooks) {
+  setupTest(hooks);
 
-    test('it works', async function (assert) {
-      class Test {
-        _rand = useFunction(this, () => Math.random());
-        rand = shared(this, () => this._rand);
-      }
+  test('it works', async function (assert) {
+    class Test {
+      @shared num = 2;
+    }
 
-      let foo1 = new Test();
-      let foo2 = new Test();
+    let foo1 = new Test();
+    let foo2 = new Test();
 
-      setOwner(foo1, this.owner);
-      setOwner(foo2, this.owner);
+    assert.equal(foo1.num, foo2.num);
 
-      let rand1 = foo1.rand.value;
+    foo1.num = 3;
+    assert.equal(foo1.num, foo2.num);
 
-      await settled();
-
-      assert.equal(foo2.rand.value, rand1);
-    });
+    foo2.num = 7;
+    assert.equal(foo1.num, foo2.num);
+  });
 });
